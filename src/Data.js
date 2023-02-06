c29b2f7f90f4783d1994150075992bcc38e99c9b
Pasindu Shyaminda <it21016202@my.sliit.lk>
2023-02-06 22:05:32 +0530

    cityIDRreadFromFile

--- src/Data.js
+++ src/Data.js
@@ -10,9 +10,9 @@ function Data(){
     })}
 
     const time = new Date().getTime();
-    const cityCodes = [1248991,1850147,2644210,2988507,2147714,4930956,1796236,3143244];
+    //const cityCodes = [1248991,1850147,2644210,2988507,2147714,4930956,1796236,3143244];
 
-    axios.get(`http://api.openweathermap.org/data/2.5/group?id=${cityCodes}&units=metric&appid=${process.env.REACT_APP_API_KEY_1}`)
+    axios.get(`http://api.openweathermap.org/data/2.5/group?id=${cityArr}&units=metric&appid=${process.env.REACT_APP_API_KEY_1}`)
     .then((res)=>{
         localStorage.setItem("weather", JSON.stringify(res.data.list));
         localStorage.setItem("weather_expire", time + 300000);
